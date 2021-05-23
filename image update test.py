9eb5ac5b3bba15ecfdf22fc1cfd3934cb4024e47
Konstantin-Lasiy <kostyalasiy@gmail.com>
2021-05-23 11:47:05 -0600

    ^another try at the syntax

--- image update test.py
+++ image update test.py
@@ -38,7 +38,7 @@ def update_image(epd, number):
 def callback_minute(context: CallbackContext):
         global last_message_time
         global number
-        epd = context.epd
+        epd = context['epd']
         update_image(epd, number)
         number = number + 1
         last_message_time = datetime.datetime.now()
