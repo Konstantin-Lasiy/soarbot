ffd95887274273fc64bfcf5a45fae7d1e1a5c8ce
Konstantin-Lasiy <kostyalasiy@gmail.com>
2021-05-23 11:01:13 -0600

    took out the  init stuff

--- image update test.py
+++ image update test.py
@@ -69,27 +69,26 @@ def main():
 
 
 def main_simple():
+    logging.info('In update_image')
+    epd = epd7in5_V2.EPD()
+    logging.info('starting init')
+    try:
+        epd.init()
+    except:
+        print('epd.init() failed')
+    logging.info('starting Clear')
+    try:
+        epd.Clear()
+    except:
+        print("epd.Clear failed")
+    logging.info('done with Clear')
+    screen_w = epd.width
+    screen_h = epd.height
     for number in range(3):
-        logging.info('In update_image')
-        epd = epd7in5_V2.EPD()
-        logging.info('starting init')
-        try:
-            epd.init()
-        except:
-            print('epd.init() failed')
-        logging.info('starting Clear')
-        try:
-            epd.Clear()
-        except:
-            print("epd.Clear failed")
-        logging.info('done with Clear')
-        screen_w = epd.width
-        screen_h = epd.height
         image = Image.new('1', (screen_h, screen_w), 255)
         logging.info('Drawing image')
         draw = ImageDraw.Draw(image)
         draw_station_data(draw, number, 0 + 10, screen_h - 10, screen_w - 10, 0 + 10)
-
         epd.display(epd.getbuffer(image))
         logging.info('Go to sleep')
         epd.sleep()
