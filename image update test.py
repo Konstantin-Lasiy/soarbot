1d0e56de38488b45f025d29eb9d0fc68596500c7
Konstantin-Lasiy <kostyalasiy@gmail.com>
2021-05-23 10:59:37 -0600

    Created a simple main option

--- image update test.py
+++ image update test.py
@@ -68,7 +68,34 @@ def main():
     updater.idle()
 
 
+def main_simple():
+    for number in range(3):
+        logging.info('In update_image')
+        epd = epd7in5_V2.EPD()
+        logging.info('starting init')
+        try:
+            epd.init()
+        except:
+            print('epd.init() failed')
+        logging.info('starting Clear')
+        try:
+            epd.Clear()
+        except:
+            print("epd.Clear failed")
+        logging.info('done with Clear')
+        screen_w = epd.width
+        screen_h = epd.height
+        image = Image.new('1', (screen_h, screen_w), 255)
+        logging.info('Drawing image')
+        draw = ImageDraw.Draw(image)
+        draw_station_data(draw, number, 0 + 10, screen_h - 10, screen_w - 10, 0 + 10)
+
+        epd.display(epd.getbuffer(image))
+        logging.info('Go to sleep')
+        epd.sleep()
+
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    main()
+    main_simple()
