a4996c523e4367dedbc9867efe215d2b85c39564
Konstantin-Lasiy <kostyalasiy@gmail.com>
2021-05-23 11:59:25 -0600

    adding context.job.cotext to get context

--- image update test.py
+++ image update test.py
@@ -38,7 +38,7 @@ def update_image(epd, number):
 def callback_minute(context: CallbackContext):
         global last_message_time
         global number
-        print(context)
+        print(context.job.context)
         epd = context['epd']
         update_image(epd, number)
         number = number + 1
