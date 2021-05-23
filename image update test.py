2958cdc62ca12c8beeef412d5b5f00f7b6a59c70
Konstantin-Lasiy <kostyalasiy@gmail.com>
2021-05-23 12:00:47 -0600

    finally accessed the context

--- image update test.py
+++ image update test.py
@@ -38,8 +38,7 @@ def update_image(epd, number):
 def callback_minute(context: CallbackContext):
         global last_message_time
         global number
-        print(context.job.context)
-        epd = context['epd']
+        epd = context.job.context['epd']
         update_image(epd, number)
         number = number + 1
         last_message_time = datetime.datetime.now()
