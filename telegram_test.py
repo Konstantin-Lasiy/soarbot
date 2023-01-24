e8799981a135e40b7ebc1bf923ffa7ddfe4dc223
Konstantin-Lasiy <kostyalasiy@gmail.com>
2023-01-23 22:48:55 -0700

    added telegram_test file

--- telegram_test.py
+++ telegram_test.py
@@ -1,11 +1,27 @@
 import asyncio
 import telegram
 import config
+import logging 
+from logging.handlers import RotatingFileHandler
 
-async def main():
-    bot = telegram.Bot(config.telegram_token)
+async def main(dude, bot):
     async with bot:
-        await bot.send_message(text='Hi John!', chat_id='-1001802599929')
+        await bot.send_message(text=f'Hi {dude}!', chat_id='-1001802599929')
         
 if __name__ == '__main__':
-    asyncio.run(main())
+    bot = telegram.Bot(config.telegram_token)
+    asyncio.run(main('Costa',bot))
+    
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+    my_handler = RotatingFileHandler('std.log', mode='a', maxBytes=5*1024*1024, 
+                                    backupCount=2, encoding=None, delay=False)
+    my_handler.setFormatter(log_formatter)
+    my_handler.setLevel(logging.INFO)
+
+    app_log = logging.getLogger('root')
+    app_log.setLevel(logging.INFO)
+    app_log.addHandler(my_handler)
+
+    while True:
+        #app_log.info("data")
+        logging.info('daytime: hi')
