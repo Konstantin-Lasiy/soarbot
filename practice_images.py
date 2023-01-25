e9fb2b7c027d3aa233bb23382b9f0feda97e1f67
Konstantin-Lasiy <kostyalasiy@gmail.com>
2023-01-24 18:55:15 -0700

    Created practice_images file

--- practice_images.py
+++ practice_images.py
@@ -13,25 +13,15 @@ def format_message(station_data, rows=6, html=True):
     if html:
         message = "<pre>"  # ""TIME  |  WIND SPEEDgGUST | WIND DIRECTION \n"
     else:
-        message = "TIME  |  WIND SPEEDgGUST | WIND DIRECTION \n"
+        message = "       Speed   Dir \n"
     for index, row in station_data.tail(rows).iloc[::-1].iterrows():
-        message += ('{:%H:%M} | {}{:1.0f}g{:1.0f} | {} \n'.format(
-            row['date_time'], " " * 0,
-            row['wind_speed_set_1'],
-            row['wind_gust_set_1'],
-            row['wind_cardinal_direction_set_1d']))
+        #wind_combo = f'{row["wind_speed_set_1"]:1.0f}g{row["wind_gust_set_1"]:1.0f}'
+        message += '{:%H:%M} {:>3.0f}g{:<2.0f}   {:<5} \n'.format(
+            row['date_time'], 
+            row["wind_speed_set_1"],row["wind_gust_set_1"],
+            row['wind_cardinal_direction_set_1d'])
     if html:
         message += '</pre>'
-    else:
-        message += '''       __,-, 
-    ,-'  \  \ 
-  ,'   \  \  \ 
- /  \   \ _,==;
-/ \  \.,="|  |
-\  \ /'   | |
- \  /'--,_o_|
-  `<'_,---|#, 
-'''
     return message
 
 
@@ -54,23 +44,28 @@ def get_station_data(lookback_minutes=30):
     latest_recordings_df['wind_gust_set_1'] *= 1.15078
     return latest_recordings_df
 
+
 def draw_station_data(draw, station_data, left, top, right, bottom):
     text = format_message(station_data, rows=10, html=False)
-    font18 = ImageFont.truetype('./fonts/mononoki-Regular.ttf', 22)
+    font18 = ImageFont.truetype('./fonts/mononoki-Regular.ttf', 20)
     draw.text((left, top), text, font=font18)
 
 station_data = get_station_data(60)
 
 
 
-image = Image.new('1', (screen_h, screen_w), 255)
+image = Image.new('1', (screen_w, screen_h), 255)
 
 #Create Image object
 im2 = Image.open("paraglider-paragliding.bmp")
-image.paste(im2, (100, 200))
+vertical_push = 90
+image.paste(im2, (100, 300+vertical_push))
 
 draw = ImageDraw.Draw(image)
-draw_station_data(draw, station_data, 120, 10, screen_w - 10, 0 + 10)
+draw_station_data(draw, station_data, 140, 20 + vertical_push, screen_w - 10, 0 + 10)
+#draw.line((205, 20+ vertical_push) + (205,260+ vertical_push), fill='black') # left vertical
+#draw.line((290, 20+ vertical_push) + (290,260+ vertical_push), fill='black') # right v
+draw.line((139, 42+ vertical_push, 339, 42+ vertical_push), fill='black', width=3) # horizontal
 
 #Show image
 image.show()
