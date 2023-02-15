b9a84e6597d1f66e6f6394a28c775bd8e794e0a2
Konstantin-Lasiy <kostyalasiy@gmail.com>
2023-02-15 13:15:34 -0700

    V2.0 - working asyncio

--- SS_soarbot.py
+++ SS_soarbot.py
@@ -12,11 +12,20 @@ from astral.sun import sun
 from astral import LocationInfo
 from PIL import Image, ImageDraw, ImageFont
 import asyncio
-
+import io
+import matplotlib.pyplot as plt
 import epd7in5_V2
+import sys
+from async_timeout import timeout
 
 absolute_path = os.path.dirname(__file__)
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    app_log.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
+sys.excepthook = handle_exception
 
 def get_station_data(lookback_minutes=30):
     request_string = 'https://api.synopticdata.com/v2/stations/timeseries?' \
@@ -51,7 +60,7 @@ def check_wind(station_data):
     wind_dir_is_acceptable = ((last_3_wind_directions > bottom_wind_dir_value) &
                               (last_3_wind_directions < top_win_dir_value)).all().iloc[0]
     wind_is_acceptable = wind_speed_is_acceptable and wind_dir_is_acceptable
-    return wind_is_acceptable
+    return wind_is_acceptable, wind_dir_is_acceptable, wind_speed_is_acceptable
 
 
 def check_rain(station_data):
@@ -91,29 +100,25 @@ def check_midday():
 
 
 def check_all_conditions(station_data, winter):
-    wind_is_acceptable = check_wind(station_data)
+    wind_is_acceptable,_,_ = check_wind(station_data)
     is_raining = check_rain(station_data)
     strong_gusts = check_for_strong_gusts(station_data)
     daytime = check_daytime()
 
-    # In the summer one needs to include midday times.
+    # In the summer midday hours should be ignored. In the winter midday flying is fine
     if not winter:
         midday = check_midday()
     else:
-        midday = True
+        midday = False
     all_conditions_are_right = wind_is_acceptable and not is_raining and not strong_gusts and daytime and not midday
-    app_log.info('wind: {wind}'.format(wind=wind_is_acceptable))
-    app_log.info('rain: {rain}'.format(rain=is_raining))
-    app_log.info('gusts: {gusts}'.format(gusts=strong_gusts))
-    app_log.info('daytime: {daytime}'.format(daytime=daytime))
-    app_log.info('midday: {midday}'.format(midday=midday))
-    app_log.info('all_conditions: {all_conditions}'.format(all_conditions=all_conditions_are_right))
-    if all_conditions_are_right:
-        return True
-    else:
-        return False
-relative_path = "src/lib"
-full_path = os.path.join(absolute_path, relative_path)
+    # app_log.info('wind: {wind}'.format(wind=wind_is_acceptable))
+    # app_log.info('rain: {rain}'.format(rain=is_raining))
+    # app_log.info('gusts: {gusts}'.format(gusts=strong_gusts))
+    # app_log.info('daytime: {daytime}'.format(daytime=daytime))
+    # app_log.info('midday: {midday}'.format(midday=midday))
+    # app_log.info('all_conditions: {all_conditions}'.format(all_conditions=all_conditions_are_right))
+    return all_conditions_are_right
+
 
 def play_sound():
     relative_path = 'Alarm Alert Effect-SoundBible.com-462520910.mp3'
@@ -145,50 +150,99 @@ def latest_readings(bot, job):
 
 
 def format_message(station_data, rows=6, html=True):
+    station_data['wind_cardinal_direction_set_1d'].fillna('-', inplace=True)
     if html:
         message = "<pre>"  # ""TIME  |  WIND SPEEDgGUST | WIND DIRECTION \n"
     else:
-        message = "\n"
+        message = "       Speed   Dir \n"
     for index, row in station_data.tail(rows).iloc[::-1].iterrows():
-        message += ('{:%H:%M} | {}{:2.0f}g{:1.0f} | {} \n'.format(
-            row['date_time'], " " * 0,
-            row['wind_speed_set_1'],
-            row['wind_gust_set_1'],
-            row['wind_cardinal_direction_set_1d']))
+        message += '{:%H:%M} {:>3.0f}g{:<2.0f}   {:<5} \n'.format(
+            row['date_time'], 
+            row["wind_speed_set_1"],row["wind_gust_set_1"],
+            row['wind_cardinal_direction_set_1d'])
     if html:
         message += '</pre>'
-    else:
-        message += ''
     return message
+
 
 
 def draw_station_data(draw, station_data, left, top, right, bottom):
     text = format_message(station_data, rows=10, html=False)
-    relative_path = './fonts/mononoki-Regular.ttf'
-    path_to_font = os.path.join(absolute_path, relative_path)
-    font18 = ImageFont.truetype(path_to_font, 22)
+    font18 = ImageFont.truetype(os.path.join(absolute_path, './fonts/mononoki-Regular.ttf'), 25)
     draw.text((left, top), text, font=font18)
 
+def draw_status(image, status, x, y):
+    if status:
+        checkmark = Image.open(os.path.join(absolute_path, "images/ok.bmp"))
 
+        image.paste(checkmark, (x,y))
+    else:
+        cross = Image.open(os.path.join(absolute_path, "images/cross.bmp"))
+        image.paste(cross,(x,y))
+
+def draw_speed_chart(image,station_data, x, y):
+    import matplotlib.dates as mdates
+    myFmt = mdates.DateFormatter('%H:%M')
+    plot_data = station_data.set_index('date_time')['wind_speed_set_1'].tail(10)
+    plt.rcParams["figure.figsize"] = (3.5,2)
+    fig, ax = plt.subplots(1,1)
+    plt.plot(plot_data)
+    ax.xaxis.set_major_formatter(myFmt)
+    xlocator = mdates.MinuteLocator(byminute=[0,10,20,30,40,50], interval = 1)
+    ax.xaxis.set_major_locator(xlocator)
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    im = Image.open(img_buf)
+    image.paste(im, (x, y))
+    img_buf.close()
+    plt.close(fig)
+
+def draw_statuses(draw,image,station_data,y_displacement):
+    _,wind_dir_is_acceptable,wind_speed_is_acceptable = check_wind(station_data)
+    is_raining = check_rain(station_data)
+    strong_gusts = check_for_strong_gusts(station_data)
+    font18 = ImageFont.truetype(os.path.join(absolute_path, './fonts/mononoki-Regular.ttf'), 18)
+    draw.text((120,600+y_displacement), 'SPEED', font=font18)
+    draw.text((110,700+y_displacement), 'DIRECTION', font=font18)
+    draw.text((286,600+y_displacement), 'GUSTS', font=font18)
+    draw.text((290,700+y_displacement), 'RAIN', font=font18)
+    draw_status(image, wind_speed_is_acceptable,120,620+y_displacement)
+    draw_status(image, wind_dir_is_acceptable,120,720+y_displacement)
+    draw_status(image, not(strong_gusts),285,620+y_displacement)
+    draw_status(image, not(is_raining),285,720+y_displacement)
+    line1_x = 230
+    line1_y = (600+y_displacement,780+y_displacement)
+    draw.line((line1_x, line1_y[0]) + (line1_x,line1_y[1]), fill='black', width = 5) # l
+    line2_x = (100, 350)
+    line2_y = 685 +y_displacement
+    draw.line((line2_x[0], line2_y) + (line2_x[1],line2_y), fill='black', width = 5) # l
+
+    
+    
 def update_image(epd, station_data, good_conditions):
-    app_log.info('In update_image')
-    app_log.info('starting init')
+    #app_log.info('In update_image')
+    #app_log.info('starting init')
     try:
         epd.init()
+        # app_log.info(f'Init done')
     except:
         print('epd.init() failed')
     screen_w = epd.width
     screen_h = epd.height
     image = Image.new('1', (screen_h, screen_w), 255)
-    app_log.info('Drawing image')
+    #app_log.info('Drawing image')
     
     draw = ImageDraw.Draw(image)
-    draw_station_data(draw, station_data, 120, 40, screen_w - 10, 0 + 10)
-    if good_conditions:
-        im2 = Image.open(os.path.join(absolute_path, "paraglider-paragliding.bmp"))
-        image.paste(im2, (80, 300))
+    y_displacement = 40
+    draw_speed_chart(image,station_data, 56,310+y_displacement)
+
+    table_y_position = 26
+    draw.rounded_rectangle([100,table_y_position+y_displacement, 371, table_y_position+295+y_displacement], radius=0, fill=None, outline='black', width=3)
+    draw_statuses(draw,image,station_data, -40)
+    draw_station_data(draw, station_data, 110, table_y_position+2 + y_displacement, screen_w - 10, 0 + 10)
+    draw.line((100, table_y_position+29+ y_displacement, 371, table_y_position+29+ y_displacement), fill='black', width=3) # horizontal
     epd.display(epd.getbuffer(image))
-    app_log.info('Go to sleep')
+    app_log.info(f'Update Image done. Setting epd to sleep')
     epd.sleep()
 
 
@@ -197,6 +251,10 @@ async def repeated_job(bot, epd, winter):
     lookback_minutes = 120
     time_since_last_message = datetime.datetime.now() - last_message_time
     station_data = get_station_data(lookback_minutes)
+    if len(station_data)== 0:
+        app_log.error('station data empty... waiting a minute to retry')
+        time.sleep(60)
+        return
     all_parameters_met = check_all_conditions(station_data, winter)
     update_image(epd, station_data, all_parameters_met)
     #if True: #morning
@@ -209,20 +267,29 @@ async def repeated_job(bot, epd, winter):
                                  text=message,
                                  parse_mode='HTML')
         last_message_time = datetime.datetime.now()
+    app_log.info('Job ran, going to sleep')
     time.sleep(config.sleep_time)
+    app_log.info('Woke up.')
 
 async def send_start_message(bot):
     async with bot:
         await bot.send_message(text='Script started.', chat_id='-1001802599929')
 
-def main():
+async def send_error(bot, error):
+    async with bot:
+        await bot.send_message(text =f'{error}', chat_id='-1001802599929')
+
+
+
+async def main():
     global last_message_time
     global run_time
     winter = True
 
     try:
         bot = telegram.Bot(config.telegram_token)
-        asyncio.run(send_start_message(bot))
+        async with bot:
+            await send_start_message(bot)
         last_message_time = datetime.datetime.now() - datetime.timedelta(hours=5)
         epd = epd7in5_V2.EPD()
         
@@ -232,16 +299,25 @@ def main():
         except:
             print('epd.init() failed')
 
-        app_log.info('starting Clear')
+        #app_log.info('starting Clear')
         try:
             epd.Clear()
         except:
             print("epd.Clear failed")
-        app_log.info('done with Clear')
+        #app_log.info('done with Clear')
         epd.sleep()
         
         while True:
-            asyncio.run(repeated_job(bot,epd,winter))
+            try:
+                async with timeout(120):
+                    await repeated_job(bot,epd,winter)
+            except Exception as e:
+                app_log.info(f'Exception {e} occured, trying to send to Telegram.')
+                try:
+                    await send_error(bot, e)
+                except Exception as e:
+                    app_log.critical(f'Exceptiong {e} occured when trying to send error to bot')
+                time.sleep(120)
 
     except KeyboardInterrupt:
         app_log.info("ctrl + c:")
@@ -249,10 +325,8 @@ def main():
         exit()
     
     except Exception as e:
-        async def send_error(bot, error):
-            async with bot:
-                await bot.send_message(text =f'{error}', chat_id='-1001802599929')
-        asyncio.run(send_error(bot, e))
+        app_log.info(f'Exception {e} occured')
+        await send_error(bot, e)
 
 if __name__ == "__main__":
     log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
@@ -266,4 +340,4 @@ if __name__ == "__main__":
     app_log.setLevel(logging.INFO)
     app_log.addHandler(my_handler)
 
-    main()
+    asyncio.run(main())
