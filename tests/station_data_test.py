58098332e560750bd01c19c69a331f83d8783674
Costa Lasiy <kostyalasiy@gmail.com>
2025-08-13 05:16:53 +0000

    REMOCED old tests

--- tests/station_data_test.py
+++
@@ -1,5 +0,0 @@
-import run_screen as ss
-
-
-if __name__ == "__main__":
-    print(ss.get_station_data())
--- tests/telegram_test.py
+++
@@ -1,4 +0,0 @@
-import run_screen as SS
-from configs import config
-
-SS.send_to_telegram('Test run successfully!', config.test_chat_id)
